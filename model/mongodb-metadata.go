3274127bc8fdb9bcd20ed4aef9a07356505b5a17
Hafid Dian Nurfaujan Ahat <hafiddna@gmail.com>
2025-07-15 08:14:26 +0700

    Fix MongoDB models

--- model/mongodb-metadata.go
+++ model/mongodb-metadata.go
@@ -1,14 +1,14 @@
 package model
 
 import (
-	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/v2/bson"
 )
 
 type MongoDBMetadata struct {
-	ID        primitive.ObjectID     `json:"id" bson:"_id,omitempty"`
-	CreatedAt primitive.Timestamp    `json:"created_at" bson:"created_at"`
-	UpdatedAt primitive.Timestamp    `json:"updated_at" bson:"updated_at"`
-	DeletedAt primitive.Timestamp    `json:"deleted_at" bson:"deleted_at,omitempty"`
+	ID        bson.ObjectID          `json:"id" bson:"_id,omitempty"`
+	CreatedAt bson.Timestamp         `json:"created_at" bson:"created_at"`
+	UpdatedAt bson.Timestamp         `json:"updated_at" bson:"updated_at"`
+	DeletedAt bson.Timestamp         `json:"deleted_at" bson:"deleted_at,omitempty"`
 	Metadata  map[string]interface{} `json:"metadata" bson:"metadata"`
 }
 
