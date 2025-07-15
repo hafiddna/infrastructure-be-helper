636bffc1e50e8e362bff5328835b2a0e832cd14b
Hafid Dian Nurfaujan Ahat <hafiddna@gmail.com>
2025-07-15 09:20:34 +0700

    Fix MongoDB structs

--- model/user-profile.go
+++ model/user-profile.go
@@ -1,8 +1,14 @@
 package model
 
+import "go.mongodb.org/mongo-driver/v2/bson"
+
 type UserProfile struct {
-	MongoDBMetadata
-	UserID   string `json:"user_id" bson:"user_id"`
-	FullName string `json:"full_name" bson:"full_name,omitempty"`
-	NickName string `json:"nick_name" bson:"nick_name,omitempty"`
+	ID        bson.ObjectID          `json:"id" bson:"_id,omitempty"`
+	UserID    string                 `json:"user_id" bson:"user_id"`
+	FullName  string                 `json:"full_name" bson:"full_name,omitempty"`
+	NickName  string                 `json:"nick_name" bson:"nick_name,omitempty"`
+	CreatedAt bson.DateTime          `json:"created_at" bson:"created_at"`
+	UpdatedAt bson.DateTime          `json:"updated_at" bson:"updated_at"`
+	DeletedAt *bson.DateTime         `json:"deleted_at" bson:"deleted_at,omitempty"`
+	Metadata  map[string]interface{} `json:"metadata" bson:"metadata,omitempty"`
 }
--- model/user-setting.go
+++ model/user-setting.go
@@ -1,6 +1,12 @@
 package model
 
+import "go.mongodb.org/mongo-driver/v2/bson"
+
 type UserSetting struct {
-	MongoDBMetadata
-	UserID string `json:"user_id" bson:"user_id"`
+	ID        bson.ObjectID          `json:"id" bson:"_id,omitempty"`
+	UserID    string                 `json:"user_id" bson:"user_id"`
+	CreatedAt bson.DateTime          `json:"created_at" bson:"created_at"`
+	UpdatedAt bson.DateTime          `json:"updated_at" bson:"updated_at"`
+	DeletedAt *bson.DateTime         `json:"deleted_at" bson:"deleted_at,omitempty"`
+	Metadata  map[string]interface{} `json:"metadata" bson:"metadata,omitempty"`
 }
